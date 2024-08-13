0929824402bad47207de850bbcd9be11c0be1798
DejMar <marjanovic.dejan@gmail.com>
2024-08-13 22:50:09 +0200

    adding get request to verify user is added

--- tests/API/updateUser.spec.js
+++ tests/API/updateUser.spec.js
@@ -36,72 +36,82 @@ test.describe('API - Manipulating users', () => {
         expect(response.status()).toBe(201);
         const responseBody = await response.json();
         expect(responseBody).toEqual(expect.objectContaining(userData));
+
+        // Get response to check if user is added
+        const getResponse = await request.get(`${BASE_URL_API}/contacts/${responseBody._id}`, {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        });
+        expect(getResponse.status()).toBe(200);
+        const getResponseBody = await getResponse.json();
+        expect(getResponseBody).toEqual(expect.objectContaining(userData));
     })
 
     test('API02 - Verify Updated user', async ({ request }) => {
-    const token = await loginPage.retriveToken();
-    const userData = await sharedSteps.generateUserData();
+        const token = await loginPage.retriveToken();
+        const userData = await sharedSteps.generateUserData();
 
-    const createResponse = await request.post(`${BASE_URL_API}/contacts`, {
-        headers: {
-            "Authorization": `Bearer ${token}`,
-            "Content-Type": "application/json"
-        },
-        data: userData
-    });
-    expect(createResponse.status()).toBe(201);
-    const createdUser = await createResponse.json();
+        const createResponse = await request.post(`${BASE_URL_API}/contacts`, {
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+            },
+            data: userData
+        });
+        expect(createResponse.status()).toBe(201);
+        const createdUser = await createResponse.json();
 
-    // Update the created user
-    const updatedUserData = {
-        ...userData,
-        "firstName": faker.person.firstName(),
-        "lastName": faker.person.lastName(),
-        "email": faker.internet.email().toLowerCase()
-    };
+        // Update the created user
+        const updatedUserData = {
+            ...userData,
+            "firstName": faker.person.firstName(),
+            "lastName": faker.person.lastName(),
+            "email": faker.internet.email().toLowerCase()
+        };
 
-    const updateResponse = await request.put(`${BASE_URL_API}/contacts/${createdUser._id}`, {
-        headers: {
-            "Authorization": `Bearer ${token}`,
-            "Content-Type": "application/json"
-        },
-        data: updatedUserData
-    });
-    expect(updateResponse.status()).toBe(200);
-    const updatedUser = await updateResponse.json();
-    expect(updatedUser).toEqual(expect.objectContaining(updatedUserData));
+        const updateResponse = await request.put(`${BASE_URL_API}/contacts/${createdUser._id}`, {
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+            },
+            data: updatedUserData
+        });
+        expect(updateResponse.status()).toBe(200);
+        const updatedUser = await updateResponse.json();
+        expect(updatedUser).toEqual(expect.objectContaining(updatedUserData));
     })
 
     test('API03 - Verify Deleted user', async ({ request }) => {
-    const token = await loginPage.retriveToken();
-    const userData = await sharedSteps.generateUserData();
+        const token = await loginPage.retriveToken();
+        const userData = await sharedSteps.generateUserData();
 
-    const createResponse = await request.post(`${BASE_URL_API}/contacts`, {
-        headers: {
-            "Authorization": `Bearer ${token}`,
-            "Content-Type": "application/json"
-        },
-        data: userData
-    });
-    expect(createResponse.status()).toBe(201);
-    const createdUser = await createResponse.json();
+        const createResponse = await request.post(`${BASE_URL_API}/contacts`, {
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+            },
+            data: userData
+        });
+        expect(createResponse.status()).toBe(201);
+        const createdUser = await createResponse.json();
 
-    // Delete the created user
-    const deleteResponse = await request.delete(`${BASE_URL_API}/contacts/${createdUser._id}`, {
-        headers: {
-            "Authorization": `Bearer ${token}`,
-            "Content-Type": "application/json"
-        }
-    });
-    expect(deleteResponse.status()).toBe(200);
+        // Delete the created user
+        const deleteResponse = await request.delete(`${BASE_URL_API}/contacts/${createdUser._id}`, {
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+            }
+        });
+        expect(deleteResponse.status()).toBe(200);
 
-    // Verify the user is deleted
-    const getResponse = await request.get(`${BASE_URL_API}/contacts/${createdUser._id}`, {
-        headers: {
-            "Authorization": `Bearer ${token}`,
-            "Content-Type": "application/json"
-        }
-    });
-    expect(getResponse.status()).toBe(404);
+        // Verify the user is deleted
+        const getResponse = await request.get(`${BASE_URL_API}/contacts/${createdUser._id}`, {
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+            }
+        });
+        expect(getResponse.status()).toBe(404);
     })
 })

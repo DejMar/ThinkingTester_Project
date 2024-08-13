69ba6906f74f9207a98f4845da1deee0098e0b89
DejMar <marjanovic.dejan@gmail.com>
2024-08-13 17:54:50 +0200

    Update login test cases

--- page-object/LoginPage.js
+++ page-object/LoginPage.js
@@ -12,6 +12,10 @@ export class LoginPage {
         this.signUpFirstName = page.getByPlaceholder('First Name')
         this.signUpLastName = page.getByPlaceholder('Last Name')
         this.signUpPassword = page.getByPlaceholder('Password')
+        this.logoutButton = page.getByRole('button', { name: 'Logout' })
+        this.emailField = page.getByPlaceholder('Email')
+        this.passwordField = page.getByPlaceholder('Password')
+
     }
     openURL = async () => {
         await this.page.goto('/')
@@ -24,6 +28,21 @@ export class LoginPage {
         await this.emailInput.fill(loginDetails.email)
         await this.signUpPassword.fill(loginDetails.password)
         await this.submitButton.click()
+    }
+
+    loginWithRegisteredUser = async () => {
+        await this.emailField.fill(loginDetails.email)
+        await this.passwordField.fill(loginDetails.password)
+        await this.submitButton.click()
+    }
+
+    confirmLogoutButtonDisplayed = async () => {
+        await this.logoutButton.waitFor({ state: 'visible' });
+        return this.logoutButton.isVisible();
+    }
+
+    clickOnLogoutButton = async () => {
+        await this.logoutButton.click()
     }
 
     retriveToken = async () => {
--- tests/E2E/login.spec.js
+++ tests/E2E/login.spec.js
@@ -1,22 +1,38 @@
 import { LoginPage } from "../../page-object/LoginPage"
 import { test } from '@playwright/test'
+import { SharedSteps } from "../../helper/sharedSteps"
+import { TestStep } from "../../helper/TestStep"
 
 test.describe('Login tests', () => {
   let loginPage
+  let sharedSteps
+  let testStep
+
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page)
+    sharedSteps = new SharedSteps(page)
+    testStep = new TestStep()
   })
 
   test.afterEach(async ({ page }, testInfo) => {
     await sharedSteps.takeScreenshotOnFailure(page, testInfo);
+    await sharedSteps.saveTestSteps(testInfo.title, testStep.getSteps());
   });
 
-  test('Login to page', async ({ page }) => {
-    await loginPage.openURL()
+  test('TC04 - Login to page', async ({ }) => {
+    await testStep.log(loginPage.openURL(), 'Open the login URL')
   })
 
-  test('Register user', async ({ page }) => {
-    await loginPage.openURL()
-    await loginPage.registerUser()
+  test('TC05 - Register user', async ({ }) => {
+    await testStep.log(loginPage.openURL(), 'Open the login URL')
+    await testStep.log(loginPage.registerUser(), 'Register a new user')
+  })
+
+  test.only('TC06 - Login with newly registered user', async ({ }) => {
+    await testStep.log(loginPage.openURL(), 'Open the login URL')
+    await testStep.log(loginPage.registerUser(), 'Register a new user')
+    await testStep.log(loginPage.clickOnLogoutButton(), 'Click on logout button')
+    await testStep.log(loginPage.loginWithRegisteredUser(), 'Login with registered user')
+    await testStep.log(loginPage.confirmLogoutButtonDisplayed(), 'Confirm logout button is displayed')
   })
 })

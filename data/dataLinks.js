13f6435f14fe13ae9c4ac221250a6d44b08f115f
DejMar <marjanovic.dejan@gmail.com>
2024-08-17 10:35:58 +0200

    changing name for actual TC file

--- data/dataLinks.js
+++ data/dataLinks.js
@@ -3,5 +3,5 @@ export const comparingLinks = {
     actualPath: '../../ThinkingTester_project/data-result',
 
     originalFile: 'designatedUsers.json',
-    actualFile: `TC05_Add_and_verify_designated_users_table_data_${new Date().toISOString().split('T')[0]}.json`,
+    actual_TC05_File: `TC05_Add_and_verify_designated_users_table_data_${new Date().toISOString().split('T')[0]}.json`,
 }
